3979ce8749bf3e8ed897a3fa0323ab24cfb4a5b1
crone66 <marcel.croonenbroeck@gmail.com>
2017-01-11 13:58:13 +0100

    Added Projectiles

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    // Use this for initialization
+    public float Speed = 3f;
+
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position += transform.forward * Time.deltaTime * Speed;
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+        Console.WriteLine("collision");
+        Debug.Log("Collision");
+    }
+}
