0a51dafc0996d07410c8611cc86e5b62c5641fe5
Discepless <discepless@yandex.ru>
2017-01-11 21:13:05 +0100

    CameraUpdate[ItWorks!:)]
    - Camera is centered
    - Players aren't pushed anymore
    - Camera knows how many players are in
    
    Issues(I'll do it tomorrow)
    - Formula for bounds ( They'll calculated automatically )

---
+++ Assets/Scripts/CameraFollowing.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Assets;
+using UnityEngine;
+
+public class CameraFollowing : MonoBehaviour
+{
+    public List<PlayerController> Controllers;
+    void Start()
+    {
+        Controllers = new List<PlayerController>(FindObjectsOfType<PlayerController>());
+    }
+
+    void Update ()
+    {
+        //var MaxX = Controllers.Select(x => x.gameObject.transform.position.x).Max();
+        //var MaxZ = Controllers.Select(x => x.gameObject.transform.position.z).Max();
+        //var MinX = Controllers.Select(x => x.gameObject.transform.position.x).Min();
+        //var MinZ = Controllers.Select(x => x.gameObject.transform.position.z).Min();
+
+        var maxX = ConvertX(Controllers).Max();
+        var maxZ = ConvertZ(Controllers).Max();
+        var minX = ConvertX(Controllers).Min();
+        var minZ = ConvertZ(Controllers).Min();
+
+        var x = (maxX + minX) / 2;
+        var z = (maxZ + minZ) / 2;
+        transform.position = new Vector3(x,150,z);
+    }
+
+    List<float> ConvertX(List<PlayerController> controllers)
+    {
+        var xs = new List<float>();
+        foreach (var playerController in controllers)
+        {
+            xs.Add(playerController.transform.position.x);
+        }
+        return xs;
+    }
+
+    List<float> ConvertZ(List<PlayerController> controllers)
+    {
+        var ys = new List<float>();
+        foreach (var playerController in controllers)
+        {
+            ys.Add(playerController.transform.position.z);
+        }
+        return ys;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public Vector2 VerticalBounds;
+        public Vector2 HorizontalBounds;
+
+        public int PlayerId;
+        public float Speed;
+
+        private Rigidbody _physics;
+        private Transform _camera;
+        private float _xMin, _xMax, _zMin, _zMax;
+
+        void Start () {
+            _physics = GetComponent<Rigidbody>();
+            _camera = GameObject.Find("Main Camera").transform;
+        }
+
+        void FixedUpdate()
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal" + PlayerId);
+            float moveVertical = Input.GetAxis("Vertical" + PlayerId);
+            Vector3 inputVector = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            Vector3 offset = inputVector * Speed
+                             + transform.position;
+
+            _xMax = _camera.position.x + HorizontalBounds.y;
+            _zMax = _camera.position.z + HorizontalBounds.y;
+            _xMin = _camera.position.x - HorizontalBounds.x;
+            _zMin = _camera.position.z - HorizontalBounds.y;
+
+            float clampedX = Mathf.Clamp(offset.x,_xMin,_xMax);
+            float clampedZ = Mathf.Clamp(offset.z,_zMin,_zMax);
+
+            _physics.MovePosition(new Vector3(clampedX,0,clampedZ));
+        }
+
+
+    }
+
+}
