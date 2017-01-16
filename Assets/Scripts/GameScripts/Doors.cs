11e08a51b00880e572be60b53a6bbd0b3343f310
Discepless <discepless@yandex.ru>
2017-01-16 21:21:17 +0100

    Door
    - Contains Animation
    - When Player moves to close, it will be opened/closed if player is to
    far

---
+++ Assets/Scripts/GameScripts/Doors.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    private Animator animator;
+    private bool DoorOpen;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    DoorOpen = false;
+	    animator = GetComponent<Animator>();
+	    float width = transform.localScale.x;
+	    float height = transform.localScale.y;
+	    float length = transform.localScale.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void OnTriggerEnter(Collider col)
+    {
+        
+        if (col.gameObject.CompareTag("Player"))
+        {
+            print("Open!");
+            DoorOpen = true;
+            Door("OpenDoor");
+        }
+    }
+
+    
+
+    void OnTriggerExit(Collider col)
+    {
+        if (DoorOpen == true)
+        {
+            DoorOpen = false;
+            Door("CloseDoor");
+        }
+    }
+
+    void Door(string direction)
+    {
+        animator.SetTrigger(direction);
+    }
+}
