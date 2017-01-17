3854e4e92e20ba7da86c0113fa3346f95707bf61
LukasBos <lukas.bosniak@gmail.com>
2017-01-17 16:47:35 +0100

    working on ExplosiveTrap and Collapse for debris

---
+++ Assets/Scripts/Collapse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collapse : MonoBehaviour {
+
+    [SerializeField]
+    [Range(0.5f, 30)]
+    private float timeToCollapse;
+
+    private float elapsedGameTime;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        elapsedGameTime += Time.deltaTime;
+
+        if (elapsedGameTime < timeToCollapse)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
