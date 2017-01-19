ebd0183cc7e388123e72dfad225b1e400334e16a
petee <petee@LAPTOP-GOP257VS>
2017-01-19 14:30:56 +0100

    Fixes

--- Assets/Scripts/GameScripts/Player.cs
+++ Assets/Scripts/GameScripts/Player.cs
@@ -65,6 +65,8 @@ public class Player : MonoBehaviour
     public GameObject SecondaryWeapon;
     public GameObject Ability;
     public GameObject SecondaryAbility;
+
+    public bool OnIce;
     #endregion
 
     #region EventHandlers
@@ -160,10 +162,15 @@ public class Player : MonoBehaviour
     }
 
     private void FixedUpdate()
-    {
-        physics.velocity = finalVelocity + Physics.gravity;
-        finalVelocity = Vector3.zero;
-
+    {if (!OnIce)
+        {
+            physics.velocity = finalVelocity + Physics.gravity;
+            finalVelocity = Vector3.zero;
+        }
+    else
+        {
+            InputOnIce();
+        }
         Borders();
         physics.MovePosition(new Vector3(clampedX, transform.position.y, clampedZ));
         CheckOverlappingObjects();
@@ -566,4 +573,18 @@ public class Player : MonoBehaviour
         }
     }
     #endregion
+
+    public void InputOnIce()
+    {
+        physics.AddForce(finalVelocity);
+    }
+
+    public void PutOnIce()
+    {
+        OnIce = true;
+    }
+    public void PutOffIce()
+    {
+        OnIce = false;
+    }
 }
