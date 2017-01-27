7f24c4cde8b30d394ec686443ed536dc2337216e
Maksym Hlaholiev <maksym.hlaholiev@stud.hochschule-heidelberg.de>
2017-01-27 12:11:37 +0100

    assigned door sound

--- Assets/Scripts/GameScripts/Doors.cs
+++ Assets/Scripts/GameScripts/Doors.cs
@@ -1,64 +1,72 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Doors : MonoBehaviour
 {
+    private AudioSource DoorSound;
+
     private Animator animator;
     private bool DoorOpen;
-    public List <GameObject> Enemies;
+    public List<GameObject> Enemies;
     private GameObject EnemyAlive;
 
-	// Use this for initialization
-	void Start ()
-	{
-	    DoorOpen = false;
-	    animator = GetComponent<Animator>();
+    // Use this for initialization
+    private void Start()
+    {
+        //GameObject.FindGameObjectWithTag("DoorHolder").
+        DoorSound = GetComponent<AudioSource>();
+        DoorOpen = false;
+        animator = GetComponent<Animator>();
         EnemyAlive = GameObject.FindWithTag("Enemy");
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    private void Update()
     {
         CountEnemies();
-
     }
 
     private void CountEnemies()
     {
         print(Enemies.Count);
-        for (int i = Enemies.Count - 1; i > -1; i--)
+        for (var i = Enemies.Count - 1; i > -1; i--)
         {
             if (Enemies[i] == null)
                 Enemies.RemoveAt(i);
         }
     }
 
-    void OnTriggerEnter(Collider col)
+    private void OnTriggerEnter(Collider col)
     {
-        
         if (col.gameObject.CompareTag("Player") && Enemies.Count == 0)
         {
-            print("Open!");
+            PlaySound();
+            //      print("Open!");
             DoorOpen = true;
+
             Door("OpenDoor");
         }
     }
 
-    
-
-    void OnTriggerExit(Collider col)
+    private void OnTriggerExit(Collider col)
     {
         if (DoorOpen == true)
         {
+            PlaySound();
             DoorOpen = false;
+
             Door("CloseDoor");
         }
     }
 
-    void Door(string direction)
+    private void Door(string direction)
     {
         animator.SetTrigger(direction);
+    }
+
+    private void PlaySound()
+    {
+        if (DoorSound.isPlaying) DoorSound.Stop();
+        DoorSound.Play();
     }
 }
