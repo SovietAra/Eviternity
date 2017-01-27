f13ea918599f947b4022603a452856b3aaa72b10
gregor <gregor@DESKTOP-D7OSQ61>
2017-01-27 15:48:50 +0100

    SplashScreen hinzugefügt

---
+++ Assets/Scripts/MenuScripts/Neu/SplashScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using XInputDotNetPure;
+
+public class SplashScript : MonoBehaviour {
+
+    public float targetTime = 5.0f;
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+    
+	// Update is called once per frame
+	void Update ()
+    {
+        targetTime -= Time.deltaTime;
+
+        if(targetTime <= 0.0f || Input.GetButton("Submit"))
+        {
+            timerEnded();
+        }
+	}
+
+    public void timerEnded()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
