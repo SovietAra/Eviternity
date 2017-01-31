d174c86963ceb5ffc4e84775312bc1be1562b427
crone66 <marcel.croonenbroeck@gmail.com>
2017-01-31 15:06:24 +0100

    added weapon prefabs

--- Assets/Scripts/GameScripts/Boss.cs
+++ Assets/Scripts/GameScripts/Boss.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using XInputDotNetPure;
 
 public class Boss : MonoBehaviour
 {
@@ -13,13 +14,19 @@ public class Boss : MonoBehaviour
     private Ability ability;
     private MoveScript moveScript;
     private DamageAbleObject healthContainer;
-    private GameObject currentTarget = null;
+    private Player currentTarget = null;
     private float[] damageDone;
     private List<Player> playerScripts;
 
     public bool Freeze = false;
 
+    [SerializeField]
+    [Range(0, 1000)]
+    private float viewRange = 20;
 
+    [SerializeField]
+    [Range(0, 5)]
+    private float aggroMultiplicator = 2f;
 
     private void Start()
     {
@@ -29,6 +36,7 @@ public class Boss : MonoBehaviour
         ability = GetComponent<Ability>();
         healthContainer = GetComponent<DamageAbleObject>();
         moveScript = GetComponent<MoveScript>();
+        playerScripts = new List<Player>();
 
         if(healthContainer != null)
         {
@@ -79,12 +87,66 @@ public class Boss : MonoBehaviour
 
     private void CheckCurrentTarget()
     {
+        if (currentTarget == null)
+            return;
 
+        if (Vector3.Distance(currentTarget.transform.position, transform.position) > viewRange)
+        {
+            currentTarget = null;
+        }
+        else
+        {
+            CheckAggro();
+        }
+    }
+
+    private void CheckAggro()
+    {
+        float damage = damageDone[(int)currentTarget.Index];
+        for (int i = 0; i < damageDone.Length; i++)
+        {
+            if (damageDone[i] > damage * aggroMultiplicator)
+            {
+                damage = damageDone[i];
+                currentTarget = GetPlayerByIndex((PlayerIndex)i);
+
+                if(i +1 < damageDone.Length)
+                    CheckAggro();
+
+                return;
+            }
+        }
+    }
+
+    private Player GetPlayerByIndex(PlayerIndex index)
+    {
+        for (int i = 0; i < playerScripts.Count; i++)
+        {
+            if (playerScripts[i].Index == index)
+                return playerScripts[i];
+        }
+        return null;
     }
 
     private void SearchPlayer()
     {
+        int index = -1;
+        float damage = -1;
+        for (int i = 0; i < playerScripts.Count; i++)
+        {
+            if(Vector3.Distance(playerScripts[i].transform.position, transform.position) < viewRange)
+            {
+                float tempDamage = damageDone[(int)playerScripts[i].Index];
+                if(tempDamage > damage)
+                {
+                    index = i;
+                    damage = tempDamage;
+                }
+            }
+        }
 
+       // if(index >= 0 && )
+       //     currentTarget = playerScripts[i];
     }
 
     private void AttackPlayer()
