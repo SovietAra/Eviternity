1560b9a8a6ad3f4c656e76608cb7c5e3bb4b756c
gregor <bussstopper@web.de>
2017-02-06 13:45:29 +0100

    menüAnimationen hinzugefügt

--- Assets/Scripts/MenuScripts/VideoPlayer.cs
+++ Assets/Scripts/MenuScripts/VideoPlayer.cs
@@ -6,13 +6,18 @@ using UnityEngine.UI;
 public class VideoPlayer : MonoBehaviour {
 
     public MovieTexture movie;
+    public bool loop;
 
 	// Use this for initialization
 	void Start () {
         GetComponent<RawImage>().texture = movie as MovieTexture;
         movie.Play();
+        if(loop == false)
         movie.loop = false;
-	}
+
+        if (loop == true)
+            movie.loop = true;
+    }
 	
 	// Update is called once per frame
 	void Update () {
