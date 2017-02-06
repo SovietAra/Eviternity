5402d1219fb5166522b99048d39ea16353201509
Benedikt Zoeller <sephirot5@web.de>
2017-02-06 15:01:41 +0100

    Working On Camera

--- Assets/Scripts/GameScripts/Particle.cs
+++ Assets/Scripts/GameScripts/Particle.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class ParticleDestruction : MonoBehaviour {
 
+    private ParticleSystem[] particleSystems;
 	// Use this for initialization
 	void Start () {
-		
+        particleSystems = GetComponentsInChildren<ParticleSystem>();
 	}
 	
 	// Update is called once per frame
