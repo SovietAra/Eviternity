3eed85259743bcbf2165cfed2a3f514ee9b4bc5d
gregor <bussstopper@web.de>
2017-02-06 14:43:33 +0100

    restart Bug fixed

---
+++ Assets/Scripts/MenuScripts/ClearPlayersScript.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using XInputDotNetPure;
+
+public class ClearPlayersScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update() {
+
+        
+	}
+
+    public void pressExit()
+    {
+        GamePadManager.Disconnect(PlayerIndex.One);
+        GamePadManager.Disconnect(PlayerIndex.Two);
+        GamePadManager.Disconnect(PlayerIndex.Three);
+        GamePadManager.Disconnect(PlayerIndex.Four);
+
+        GlobalReferences.PlayerStates.Clear();
+    }
+}
