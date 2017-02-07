b3f3593daaaab11c4aa77552eec468b12f565a3f
crone66 <marcel.croonenbroeck@gmail.com>
2017-02-07 23:13:11 +0100

    refectoring

--- Assets/Scripts/GameScripts/ColorSwitchCheck.cs
+++ Assets/Scripts/GameScripts/ColorSwitchCheck.cs
@@ -2,10 +2,6 @@
 
 public class ColorSwitchCheck : MonoBehaviour
 {
-    //private Color objectColor = Color.green;
-
-    //private Color currentColor;
-    //private Material materialColored;
     public Material Checkpoint;
     public Material Checkpoint1;
 
@@ -15,27 +11,8 @@ public class ColorSwitchCheck : MonoBehaviour
         renderer.enabled = true;
     }
 
-    private void Update()
-    {
-    //    if (objectColor != currentColor)
-    //    {
-    //        //helps stop memory leaks
-    //        if (materialColored != null)
-    //            UnityEditor.AssetDatabase.DeleteAsset(UnityEditor.AssetDatabase.GetAssetPath(materialColored));
-
-    //        //create a new material
-    //        materialColored = new Material(Shader.Find("Transparent/Diffuse"));
-    //        materialColored.color = currentColor = objectColor;
-    //        GetComponent<Renderer>().material = materialColored;
-
-    //    }
-
-    }
-
     public void ChangeColor()
     {
-        //objectColor = Color.green;
-        //objectColor.a = 0.3f;
         GetComponent<Renderer>().material = Checkpoint1;
     }
 }
