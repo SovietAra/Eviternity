6bea7c2cfc5c07a41cf59f988785cc1f3d6267c8
gregor <bussstopper@web.de>
2017-02-07 17:30:31 +0100

    klassen können nun mit dem thumbstick ausgewählt werden

--- Assets/Scripts/GameScripts/GameInspector.cs
+++ Assets/Scripts/GameScripts/GameInspector.cs
@@ -89,6 +89,8 @@ public class GameInspector : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+
+
         Pause();
         CheckForNewPlayers();
         WinAndDefeat();
@@ -106,6 +108,14 @@ public class GameInspector : MonoBehaviour
 
             if (AllPlayerDead)
                 SpawnPlayers();
+        }
+        if (GamePad.GetState(PlayerIndex.Two).DPad.Right == ButtonState.Pressed)
+        {
+            Debug.Log(choice[1]);
+        }
+        if (GamePad.GetState(PlayerIndex.One).DPad.Right == ButtonState.Pressed)
+        {
+            Debug.Log(choice[0]);
         }
     }
 
@@ -209,7 +219,7 @@ public class GameInspector : MonoBehaviour
                 {
                     PlayerPrefab = PlayerPrefabAegis;
                 }
-
+                
 
                 GameObject gobj = SpawnPlayer(GlobalReferences.PlayerStates[i], Player.LastCheckpointPosition + new Vector3(i * 2, 1, 0));
                 if (gobj != null)
