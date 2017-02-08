e23fbf945148df37a4073b9fab157fe2d1784909
gregor <bussstopper@web.de>
2017-02-08 11:54:17 +0100

    Wechsel zwischen Credits und MainMenu angepasst, Unnötiges Script wieder importiert (doch nicht unnötig:D)

---
+++ Assets/Scripts/MenuScripts/ChangeSceneToMainMenuScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneToMainMenuScript : MonoBehaviour {
+    public float mainTime = 36.0f;
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mainTime -= Time.deltaTime;
+        if (mainTime <= 0.0f || Input.anyKey)
+        {
+            ChangeSceneToMain();
+        }
+    }
+
+    public void ChangeSceneToMain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
+
